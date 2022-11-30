e030e4c633a086f392e01310a598b71dd2fcb4f4
defijesus.eth <defijesus@buzzedbearhideout.com>
2022-11-30 12:05:15 +0000

    remove unused code in tests

--- tests/ChangeSdEmissionAdmin.t.sol
+++ tests/ChangeSdEmissionAdmin.t.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Test} from 'forge-std/Test.sol';
-import {IERC20} from 'forge-std/interfaces/IERC20.sol';
 import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
 
-import {IEmissionManager, ITransferStrategyBase, RewardsDataTypes, IEACAggregatorProxy} from '../src/interfaces/IEmissionManager.sol';
+import {IEmissionManager} from '../src/interfaces/IEmissionManager.sol';
 import {BaseTest} from './utils/BaseTest.sol';
 
 import {ChangeSdEmissionAdminPayload} from '../src/contracts/ChangeSdEmissionAdminPayload.sol';
@@ -13,7 +11,6 @@ import {ChangeSdEmissionAdminPayload} from '../src/contracts/ChangeSdEmissionAdm
 contract ChangeSdEmissionAdminTest is BaseTest {
 
   ChangeSdEmissionAdminPayload public payload;
-  address constant GUARDIAN = 0xE50c8C619d05ff98b22Adf991F17602C774F785c;
   IEmissionManager constant EMISSION_MANAGER =
     IEmissionManager(0x048f2228D7Bf6776f99aB50cB1b1eaB4D1d4cA73);
 
@@ -32,10 +29,5 @@ contract ChangeSdEmissionAdminTest is BaseTest {
     vm.startPrank(payload.NEW_EMISSION_ADMIN());
     EMISSION_MANAGER.setDistributionEnd(payload.SD(), payload.SD(), 0);
     vm.stopPrank();
-  }
-
-  function _toUint88(uint256 value) internal pure returns (uint88) {
-    require(value <= type(uint88).max, "SafeCast: value doesn't fit in 88 bits");
-    return uint88(value);
   }
 }
